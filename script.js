535ad392248cf0b03709c165b11429bdaebb4755
Dalila <56134527+Dali-codes@users.noreply.github.com>
2022-08-28 22:23:30 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstPasswordEl = document.getElementById("generate-first-password-el")
+let secondPasswordEl = document.getElementById("generate-second-password-el")
+let passwordLength = 15
+
+function generateRandomCharacter() {
+    let randomCharacter = Math.floor(Math.random() * characters.length)
+    return characters[randomCharacter]
+}
+ 
+function generateRandomPassword() {
+    let randomPasswordOne = ""
+    let randomPasswordTwo = ""
+    
+    for(let i = 0; i < passwordLength; i++) {
+        randomPasswordOne += generateRandomCharacter()
+    }
+
+    for(let i = 0; i < passwordLength; i++) {
+        randomPasswordTwo += generateRandomCharacter()
+    }
+
+    firstPasswordEl.textContent = randomPasswordOne
+    secondPasswordEl.textContent = randomPasswordTwo
+}   
